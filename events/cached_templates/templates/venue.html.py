92f320fb6ee869bd6fe3d0618268c6990ae658de
Cody Pettit <cpett26@gmail.com>
2015-04-07 12:38:05 -0600

    U.S. ID 22 and 30; 1.5 hours; Added event information to event pages

--- events/cached_templates/templates/venue.html.py
+++ events/cached_templates/templates/venue.html.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1425421353.915432
+_modified_time = 1428170581.458075
 _enable_loop = True
 _template_filename = 'C:\\Python34\\Lib\\site-packages\\django\\bin\\colonialHF\\events\\templates/venue.html'
 _template_uri = 'venue.html'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['footer', 'header', 'content']
+_exports = ['footer', 'content', 'header']
 
 
 def _mako_get_namespace(context, name):
@@ -23,18 +23,18 @@ def _mako_generate_namespaces(context):
     pass
 def _mako_inherit(template, context):
     _mako_generate_namespaces(context)
-    return runtime._inherit_from(context, 'base.htm', _template_uri)
+    return runtime._inherit_from(context, '/homepage/templates/base.htm', _template_uri)
 def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
+        def content():
+            return render_content(context._locals(__M_locals))
+        def header():
+            return render_header(context._locals(__M_locals))
         venues = context.get('venues', UNDEFINED)
         def footer():
             return render_footer(context._locals(__M_locals))
-        def header():
-            return render_header(context._locals(__M_locals))
-        def content():
-            return render_content(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'header'):
@@ -69,24 +69,12 @@ def render_footer(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_header(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def header():
-            return render_header(context)
-        __M_writer = context.writer()
-        __M_writer('\r\n    <div class="header">\r\n\r\n    </div>\r\n')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        venues = context.get('venues', UNDEFINED)
         def content():
             return render_content(context)
+        venues = context.get('venues', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n    <div class="content">\r\n      <h2 class="manage">Venue Management</h2>\r\n      <div class="text-right">\r\n        <a href="/events/venue.create/" class="btn btn-primary">Create New venue</a>\r\n      </div>\r\n      <br>\r\n      <table id="users_table" class="table table-striped table-bordered">\r\n          <tr>\r\n          <th>ID</th>\r\n          <th>Name</th>\r\n          <th>Address</th>\r\n          <th>City</th>\r\n          <th>State</th>\r\n          <th>ZIP</th>\r\n          <th>Actions</th>\r\n        </tr>\r\n')
         for venue in venues:
@@ -111,8 +99,20 @@ def render_content(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_header(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def header():
+            return render_header(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n    <div class="header">\r\n\r\n    </div>\r\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 """
 __M_BEGIN_METADATA
-{"source_encoding": "ascii", "filename": "C:\\Python34\\Lib\\site-packages\\django\\bin\\colonialHF\\events\\templates/venue.html", "uri": "venue.html", "line_map": {"66": 45, "107": 37, "72": 3, "108": 41, "78": 3, "84": 11, "27": 0, "92": 28, "93": 29, "94": 30, "95": 30, "96": 31, "97": 31, "98": 32, "99": 32, "100": 33, "101": 33, "102": 34, "39": 1, "104": 35, "105": 35, "106": 37, "103": 34, "44": 7, "114": 108, "49": 43, "91": 11, "54": 48, "60": 45}}
+{"filename": "C:\\Python34\\Lib\\site-packages\\django\\bin\\colonialHF\\events\\templates/venue.html", "source_encoding": "ascii", "uri": "venue.html", "line_map": {"66": 45, "72": 11, "108": 3, "79": 11, "80": 28, "81": 29, "82": 30, "83": 30, "84": 31, "85": 31, "86": 32, "87": 32, "88": 33, "89": 33, "90": 34, "91": 34, "92": 35, "93": 35, "94": 37, "95": 37, "96": 41, "27": 0, "102": 3, "39": 1, "44": 7, "49": 43, "114": 108, "54": 48, "60": 45}}
 __M_END_METADATA
 """
